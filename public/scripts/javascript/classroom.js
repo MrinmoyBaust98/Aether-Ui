5978228bedfcb4740c1be681e7b4731502a0ad53
Abir Sheikh <abir.sheikh.4044@gmail.com>
2021-08-31 15:57:59 +0600

    made classroom responsive

--- public/scripts/javascript/classroom.js
+++ public/scripts/javascript/classroom.js
@@ -1,11 +1,16 @@
 // jshint esversion: 6
+// variables
+let togglers = select('all', '.toggler');
+let sideBarContent = select('all', '.side-bar-content');
+
+// a simple function that selects element based on the parameter passed
 function select(n, element) {
     element = document.querySelectorAll(element);
     n = n == ('all' || 'every') ? n : isNaN(n) ? Number(n.slice(0, -2)) : n;
     return n == ('all' || 'every') ? element : element[n - 1];
 }
-let togglers = select('all', '.toggler');
-let sideBarContent = select('all', '.side-bar-content');
+
+// adds interactivity to navbar
 togglers.forEach((element, i) => {
     element.addEventListener('click', eventLog => {
         togglers.forEach(elem => {
@@ -17,4 +22,12 @@ togglers.forEach((element, i) => {
         element.classList.add('active');
         sideBarContent[i].classList.remove('w3-hide');
     });
+});
+
+// toggles chat option (smaller devices)
+let chatToggler = select('1st', '.chat-toggler');
+let chatSection = select('1st', '.chat');
+chatToggler.addEventListener('click', eventLog => {
+    chatSection.classList.toggle('hide');
+    // chatSection.classList.toggle('show');
 });
